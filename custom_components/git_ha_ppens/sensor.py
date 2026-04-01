8d4bfd6b2d60ee29c6c8262e41246d4d7870b255
manuveli <56472163+manuveli@users.noreply.github.com>
2026-04-01 15:57:12 +0200

    Bump software version to 0.4.1

--- custom_components/git_ha_ppens/sensor.py
+++ custom_components/git_ha_ppens/sensor.py
@@ -140,7 +140,7 @@ class GitHaPpensSensor(
             "name": "git-ha-ppens",
             "manufacturer": "git-ha-ppens",
             "model": "Git Version Control",
-            "sw_version": "0.4.0",
+            "sw_version": "0.4.1",
             "entry_type": "service",
             "configuration_url": "https://github.com/manuveli/git-ha-ppens",
         }
